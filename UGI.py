8bb6064f1e3ec9e5b04871bec4df0216548c3d61
Hossain Amran <amran.h063@gmail.com>
2020-05-21 00:16:36 +0200

    feat : add menu

--- UGI.py
+++ UGI.py
@@ -24,6 +24,7 @@ root = Tk()
 root.geometry("800x600")
 menu_am = Menu(root)
 root.config(menu = menu_am)
+root.title("GUI")
 
 
 #File Menu
@@ -39,6 +40,58 @@ file_menu.add_cascade(label ="Save As")
 file_menu.add_cascade(label ="Save All")
 file_menu.add_cascade(label ="print")
 file_menu.add_cascade(label = "Exit")
+
+
+
+#EDit Menu
+
+edit_menu = Menu(menu_am)
+
+#menu label
+menu_am.add_cascade(label = "Edit",menu = edit_menu)
+
+
+
+#search Menu
+
+search_menu = Menu(menu_am)
+
+#menu label
+menu_am.add_cascade(label = "Search",menu = search_menu)
+
+
+
+#tools Menu
+
+tools_menu = Menu(menu_am)
+
+#menu label
+menu_am.add_cascade(label = "Tools",menu = tools_menu)
+
+
+#search Menu
+
+search_menu = Menu(menu_am)
+
+#menu label
+menu_am.add_cascade(label = "Search",menu = search_menu)
+
+
+
+#View Menu
+
+view_menu = Menu(menu_am)
+
+#menu label
+menu_am.add_cascade(label = "View",menu = view_menu)
+
+
+#Help Menu
+
+help_menu = Menu(menu_am)
+
+#menu label
+menu_am.add_cascade(label = "Help",menu = help_menu)
 
 
 root.mainloop()
